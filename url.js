21985510a694b8dab811be75e574497e438055b4
IOPSY-CYCU <iopsy.chou@gmail.com>
2026-03-30 10:09:14 +0800

    Update url.js

--- url.js
+++ url.js
@@ -11,8 +11,6 @@ const urls = [
 	'https://www.surveycake.com/s/0bmgZ',
 	'https://www.surveycake.com/s/0bmgZ',
 	'https://www.surveycake.com/s/9XvgO',
-	'https://www.surveycake.com/s/9XvgO',
-	'https://www.surveycake.com/s/2Lzo3',
 	'https://www.surveycake.com/s/2Lzo3',
 	'https://www.surveycake.com/s/zpOZw',
 ];
