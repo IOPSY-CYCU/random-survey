ca95b325f710b80911df2d89136a5a0e067d97f6
IOPSY-CYCU <iopsy.chou@gmail.com>
2026-04-02 09:15:45 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,9 +9,6 @@
 
 const urls = [
 	'https://www.surveycake.com/s/0bmgZ',
-	'https://www.surveycake.com/s/0bmgZ',
-	'https://www.surveycake.com/s/0bmgZ',
 	'https://www.surveycake.com/s/9XvgO',
 	'https://www.surveycake.com/s/2Lzo3',
-	'https://www.surveycake.com/s/zpOZw',
 ];
